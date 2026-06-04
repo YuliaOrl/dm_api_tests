ad305f4a21c562be0693e63d87467397099dcd8b
Yulia <orbitalik32@gmail.com>
2026-06-05 00:30:18 +0300

    Добавлен отчет coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,10 +34,10 @@ options = (
 @pytest.fixture(scope="session", autouse=True)
 def setup_swagger_coverage():
     reporter = CoverageReporter(api_name='dm-api-account', host='http://185.185.143.231:5051')
+    reporter.cleanup_input_files()
     reporter.setup('/swagger/Account/swagger.json')
     yield
     reporter.generate_report()
-    reporter.cleanup_input_files()
 
 
 @pytest.fixture(scope="session", autouse=True)
